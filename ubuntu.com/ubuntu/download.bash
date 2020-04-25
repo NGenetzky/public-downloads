67be8241b5f096e12d923ce9e095a3dd7cba0a47
Nathan Genetzky <nathan@genetzky.us>
2020-04-25 15:48:22 -0500

    ubuntu: Update download script

--- ubuntu.com/ubuntu/download.bash
+++ ubuntu.com/ubuntu/download.bash
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-SCRIPTDIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)"
+SCRIPTDIR="$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)"
 
 download(){
-  local fname
-  fname0='ubuntu-19.04-desktop-amd64.iso'
-  fname1='ubuntu-19.04-live-server-amd64.iso'
+  version='20.04'
+  fname0="ubuntu-${version}-desktop-amd64.iso"
+  fname1="ubuntu-${version}-live-server-amd64.iso"
 
   cd "${SCRIPTDIR}"
-  git-annex addurl --fast \
+  git-annex addurl \
     --file "${fname0}" \
-    "http://releases.ubuntu.com/19.04/${fname0}"
-  git-annex addurl --fast \
+    "http://releases.ubuntu.com/${version}/${fname0}"
+  git-annex addurl \
     --file "${fname1}" \
-    "http://releases.ubuntu.com/19.04/${fname1}"
+    "http://releases.ubuntu.com/${version}/${fname1}"
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
